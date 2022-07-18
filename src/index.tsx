77686af9cde3b7a4c65c8648baca9ff7414d487b
j-hannes <hannes_e@gmx.de>
2022-07-18 18:52:28 +0200

    Start setting up server connection

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  createHttpLink,
+} from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:8080/modules/graphql",
+  credentials: "include",
+  fetchOptions: {
+    mode: "cors",
+  },
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: "Basic " + btoa("root:root1234"),
+      referer: "http://localhost:8080",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        admin {
+          jahia {
+            version {
+              release
+              build
+            }
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
